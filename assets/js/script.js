5059543643175a1c325a6c6cdc4df5475c732ce1
ds1242 <david.shaw1242@gmail.com>
2021-10-07 10:59:14 -0600

    setup event listeners to toggle display of elements

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+async function displayCard (event) {
+    event.preventDefault();
+    let cardDisplay = document.getElementById("cardCollapse");
+    let listDisplay = document.getElementById("explore-list");
+
+    if(cardDisplay.style.visibility === "hidden") {
+        cardDisplay.style.visibility = "visible";
+        listDisplay.style.visibility = "hidden";
+    } else {
+        cardDisplay.style.visibility = "hidden";
+        listDisplay.style.visibility = "visible";
+    }
+};
+
+
+async function hideCard (event) {
+    event.preventDefault();
+    let cardDisplay = document.getElementById("cardCollapse");
+    let listDisplay = document.getElementById("explore-list");
+
+    if(cardDisplay.style.visibility === "hidden") {
+        cardDisplay.style.visibility = "visible";
+        listDisplay.style.visibility = "hidden";
+    } else {
+        cardDisplay.style.visibility = "hidden";
+        listDisplay.style.visibility = "visible";
+    }
+};
+
+
+document.getElementById('cardCloseButton').addEventListener('click', hideCard);
+document.getElementById("button").addEventListener('click', displayCard);
