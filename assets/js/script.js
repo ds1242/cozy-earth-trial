f957f0aed09bd755776bde18714468e3f098ded6
ds1242 <david.shaw1242@gmail.com>
2021-10-07 13:15:13 -0600

    simplified script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,5 @@ async function displayCard (event) {
     }
 };
 
-
-async function hideCard (event) {
-    event.preventDefault();
-    let cardDisplay = document.getElementById("cardCollapse");
-    let listDisplay = document.getElementById("explore-list");
-
-    if(cardDisplay.style.visibility === "hidden") {
-        // cardDisplay.style.visibility = "visible";
-        listDisplay.style.visibility = "hidden";
-        $('#cardCollapse').slideDown("slow");
-    } else {
-        cardDisplay.style.visibility = "hidden";
-        listDisplay.style.visibility = "visible";
-    }
-};
-
-
-document.getElementById('cardCloseButton').addEventListener('click', hideCard);
+document.getElementById('cardCloseButton').addEventListener('click', displayCard);
 document.getElementById("button").addEventListener('click', displayCard);
